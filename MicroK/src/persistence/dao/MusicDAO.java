94c5ecc51674d520c5018793279cd20387a12145
Ettor <Ettor@160.97.136.23>
2019-01-07 12:50:03 +0100

    MicroK/build/classes/.gitignore MicroK/src/persistence/DatabaseManager.java MicroK/src/persistence/jdcb/DatabaseManagerDAOjdcb.java MicroK/WebContent/Servizi/LettoreMusicale.html MicroK/build/classes/util/MediaPlayer.class MicroK/src/util/MediaPlayer.java MicroK/src/model/Music.java MicroK/src/persistence/dao/MusicDAO.java

---
+++ MicroK/src/persistence/dao/MusicDAO.java
@@ -0,0 +1,27 @@
+package persistence.dao;
+
+import model.Music;
+
+public class MusicDAO  {
+
+	public void save(Music music) {
+	}
+	
+	public void update(Music music) {
+	}
+	
+	public void find(Music music) {
+	}
+
+	public void delete(Music music) {
+	}
+	
+	public void play (Music music) {
+	}
+
+	public void addToFavourite (Music music) {
+	}
+	
+	public void download (Music music) {
+	}
+}
